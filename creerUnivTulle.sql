754fdaec0bb60db2bd0332bc5d44765b1c9cfc51
HakimChe <144377630+HakimChe@users.noreply.github.com>
2024-03-12 12:20:45 +0100

    Add files via upload

---
+++ creerUnivTulle.sql
@@ -0,0 +1,41 @@
+drop database if exists univTulle ; 
+create database univTulle ; 
+
+use univTulle ; 
+
+create table Etudiant(
+	ine varchar(25) not null ,
+	nom varchar(25) not null ,
+	prenom varchar(25) not null ,
+	primary key(ine)
+);
+
+create table Module(
+	idModule int not null,
+	libelle varchar(25) not null,
+	primary key(idModule)
+);
+
+create table Evaluation(
+	numEval int not null,
+	dateEval varchar(25) not null,
+	duree varchar(20) not null,
+	idModule int,
+	primary key(numEval),
+	foreign key(idModule) references Module(idModule)
+);
+
+create table Suivre(
+	ine varchar,
+	idModule int,
+	primary key(ine, idModule),
+	foreign key(idModule) references Module(idModule)
+);
+
+create table Passer(
+	ine varchar,
+	numEval int, 
+	note int not null,
+	primary key(ine, numEval),
+	foreign key(numEval) references Evaluation(numEval)
+);
