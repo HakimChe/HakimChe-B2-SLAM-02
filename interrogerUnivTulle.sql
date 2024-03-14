abdb23dc0c79e76cc64c6fc0c01403ff1b2a5c99
HakimChe <144377630+HakimChe@users.noreply.github.com>
2024-03-14 12:22:59 +0100

    Add files via upload

--- interrogerUnivTulle.sql
+++ interrogerUnivTulle.sql
@@ -12,6 +12,8 @@ select numEval, avg(note) from Passer where numEval = 1 ;
 
 select Etudiant.nom, avg(note) from Passer inner join Etudiant on Passer.ine = Etudiant.ine where Etudiant.nom like "SINK" ;
 
-select Etudiant.nom, avg(note) from Passer inner join Etudiant on Passer.ine = Etudiant.ine group by Etudiant.nom ;
+select ine, avg(note) from Passer inner join Evaluation on Passer.numEval = Evaluation.numEval inner join Module on Evaluation.idModule = Module.idModule 
+where Module.libelle like "Droit%" group by Passer.ine;
 
+//Q°7 pas faite 
 
